6922cc4bb3988624090b90a9e242de3ae1a89459
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:53:37 -0500

    Adds PAZ_Graphics compatibility.

--- app.cpp
+++ app.cpp
@@ -140,7 +140,8 @@ void main()
 
 static const std::string FxaaFragSrc = 1 + R"===(
 // Linear LDR -> Antialiased gamma-corrected LDR
-const float edgeSteps[] = float[](1., 1.5, 2., 2., 2., 2., 2., 2., 2., 4.);
+const int numEdgeSteps = 10;
+const float edgeSteps[] = {1., 1.5, 2., 2., 2., 2., 2., 2., 2., 4.};
 const float edgeGuess = 8.;
 uniform sampler2D img;
 uniform sampler2D lum;
@@ -234,7 +235,7 @@ float edge_blend_fac(in sampler2D lum, in LumData l, in EdgeData e, in vec2 uv)
     vec2 puv = uvEdge + edgeStep;
     float pLumDelta = texture(lum, puv).r - edgeLum;
     bool pAtEnd = abs(pLumDelta) >= gradThresh;
-    for(int i = 0; i + 1 < edgeSteps.length() && !pAtEnd; ++i)
+    for(int i = 0; i + 1 < numEdgeSteps && !pAtEnd; ++i)
     {
         puv += edgeStep*edgeSteps[i];
         pLumDelta = texture(lum, puv).r - edgeLum;
@@ -246,7 +247,7 @@ float edge_blend_fac(in sampler2D lum, in LumData l, in EdgeData e, in vec2 uv)
     vec2 nuv = uvEdge - edgeStep;
     float nLumDelta = texture(lum, nuv).r - edgeLum;
     bool nAtEnd = abs(nLumDelta) >= gradThresh;
-    for(int i = 0; i + 1 < edgeSteps.length() && !nAtEnd; ++i)
+    for(int i = 0; i + 1 < numEdgeSteps && !nAtEnd; ++i)
     {
         nuv -= edgeStep*edgeSteps[i];
         nLumDelta = texture(lum, nuv).r - edgeLum;
@@ -259,8 +260,7 @@ float edge_blend_fac(in sampler2D lum, in LumData l, in EdgeData e, in vec2 uv)
     bool deltaSign = (pDist < nDist && pLumDelta >= 0.) || (pDist >= nDist &&
         nLumDelta >= 0.);
 
-    return float(deltaSign == (l.m - edgeLum >= 0.))*edgeSteps.length()*
-        shortestDist;
+    return float(deltaSign == (l.m - edgeLum >= 0.))*numEdgeSteps*shortestDist;
 }
 void main()
 {
