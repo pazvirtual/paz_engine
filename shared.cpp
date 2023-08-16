302c32258527d7a9759e3d3d36e5308de381c5f3
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:53:37 -0500

    Fixes asset embedding issues on macOS.

--- shared.cpp
+++ shared.cpp
@@ -1,11 +1,11 @@
 #include "shared.hpp"
 
-extern const unsigned char _binary_assets_paz_start[];
-extern const unsigned char _binary_assets_paz_end[];
+extern const unsigned char binary_assets_paz_start[];
+extern const unsigned char binary_assets_paz_end[];
 
 paz::Bytes paz::getAsset(const std::string& name)
 {
-    static const paz::Archive assets(paz::Bytes(_binary_assets_paz_start,
-        _binary_assets_paz_end));
+    static const paz::Archive assets(paz::Bytes(binary_assets_paz_start,
+        binary_assets_paz_end));
     return assets.get(name);
 }
